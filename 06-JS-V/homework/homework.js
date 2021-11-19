da34573a71be4f7b710894177115434ea7f62993
WalHernandez <walter.3034@hotmail.com>
2021-11-19 16:52:11 -0300

    HOMEWORK V terminado y bien

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -20,7 +20,7 @@ function crearUsuario() {
   return Usuario;
 }
 
-function agregarMetodo0.Prototype(Constructor) {
+function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
